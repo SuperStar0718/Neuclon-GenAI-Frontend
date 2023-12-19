334ca5a40cf96563ccec6a4051ce54918ccf27ac
SuperStar0718 <georgejohnson11305@gmail.com>
2023-12-19 11:04:05 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false, // or true for the prod file
     // backendUri: 'http://localhost:3000/api/',
     //backendUri: 'http://ip.5.104.137.23.my-advin.com:3000',
-    backendUri: 'https://neuclon-gen-api.azurewebsites.net:8080',
+    backendUri: 'https://neuclon-gen-api.azurewebsites.net',
     //neuclon-gen-api.azurewebsites.net
     // add more variables here
   };
